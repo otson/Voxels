e5d6c253ebdf073ea329dd544d339cede7af18af
otso <otso@otso-PC>
2014-05-18 21:37:29 +0300

    Improvements to chunk border vertices.

--- src/voxels/Chunk.java
+++ src/voxels/Chunk.java
@@ -25,8 +25,6 @@ public class Chunk {
     public Block[][][] blocks;
 
     public int[][] maxHeights;
-    public int[][] outerLimits;
-    public int[][] diffToChunks;
 
     public Chunk(int xOff, int zOff) {
 
@@ -34,8 +32,6 @@ public class Chunk {
         Z_OFF = zOff * (CHUNK_WIDTH);
         blocks = new Block[CHUNK_WIDTH][CHUNK_HEIGHT][CHUNK_WIDTH];
         maxHeights = new int[CHUNK_WIDTH][CHUNK_WIDTH];
-        outerLimits = new int[CHUNK_WIDTH + 2][CHUNK_WIDTH + 2];
-        diffToChunks = new int[CHUNK_WIDTH][CHUNK_WIDTH];
 
         int blockCount = 0;
         for (int x = 0; x < blocks.length; x++) {
@@ -63,29 +59,33 @@ public class Chunk {
                 maxHeights[x][z] = maxHeight;
             }
         }
-        initOuterLimits();
 
+        int difference = 0;
         for (int x = 0; x < blocks.length; x++) {
             for (int z = 0; z < blocks[x][0].length; z++) {
+                if (x == 0 || z == 0 || x == blocks.length - 1 || z == blocks[x][0].length - 1)
+                    // This shouldn't be min value of all 4 bordering block heights, since it can activate unnecessary blocks. Works for now.
+                    difference = Math.min(Math.min(Voxels.getNoise(x + X_OFF - 1, z + Z_OFF), Voxels.getNoise(x + X_OFF + 1, z + Z_OFF)), Math.min(Voxels.getNoise(x + X_OFF, z + Z_OFF + 1), Voxels.getNoise(x + X_OFF, z + Z_OFF - 1)));
                 for (int y = 0; y < blocks[x].length; y++) {
+
                     if (y == maxHeights[x][z]) {
                         blocks[x][y][z].activate();
                         activeBlocks++;
                     }
-                    
-                    else if (x == 0 && y < maxHeights[x][z] && y >= Math.min(Math.min(Voxels.getNoise(x+X_OFF-1, z+Z_OFF),Voxels.getNoise(x+X_OFF+1, z+Z_OFF)), Math.min(Voxels.getNoise(x+X_OFF, z+Z_OFF+1),Voxels.getNoise(x+X_OFF, z+Z_OFF-1)))) {
+
+                    else if (x == 0 && y < maxHeights[x][z] && y > difference) {
                         blocks[x][y][z].activate();
                         activeBlocks++;
                     }
-                    else if (x == blocks.length - 1 && y < maxHeights[x][z] && y >= Math.min(Math.min(Voxels.getNoise(x+X_OFF-1, z+Z_OFF),Voxels.getNoise(x+X_OFF+1, z+Z_OFF)), Math.min(Voxels.getNoise(x+X_OFF, z+Z_OFF+1),Voxels.getNoise(x+X_OFF, z+Z_OFF-1)))) {
+                    else if (x == blocks.length - 1 && y < maxHeights[x][z] && y > difference) {
                         blocks[x][y][z].activate();
                         activeBlocks++;
                     }
-                    else if (z == 0 && y < maxHeights[x][z] && y >= Math.min(Math.min(Voxels.getNoise(x+X_OFF-1, z+Z_OFF),Voxels.getNoise(x+X_OFF+1, z+Z_OFF)), Math.min(Voxels.getNoise(x+X_OFF, z+Z_OFF+1),Voxels.getNoise(x+X_OFF, z+Z_OFF-1)))+1) {
+                    else if (z == 0 && y < maxHeights[x][z] && y > difference) {
                         blocks[x][y][z].activate();
                         activeBlocks++;
                     }
-                    else if (z == blocks[x][y].length - 1 && y < maxHeights[x][z] && y >= Math.min(Math.min(Voxels.getNoise(x+X_OFF-1, z+Z_OFF),Voxels.getNoise(x+X_OFF+1, z+Z_OFF)), Math.min(Voxels.getNoise(x+X_OFF, z+Z_OFF+1),Voxels.getNoise(x+X_OFF, z+Z_OFF-1)))) {
+                    else if (z == blocks[x][y].length - 1 && y < maxHeights[x][z] && y > difference) {
                         blocks[x][y][z].activate();
                         activeBlocks++;
                     }
@@ -152,59 +152,4 @@ public class Chunk {
         this.vboTexHandle = vboTexHandle;
     }
 
-    private void initOuterLimits() {
-        for (int x = 0; x < outerLimits.length; x++) {
-            for (int z = 0; z < outerLimits[x].length; z++) {
-                outerLimits[x][z] = Voxels.getNoise(x + X_OFF + 1, z + Z_OFF + 1);
-            }
-        }
-        for (int x = 0; x < diffToChunks.length; x++) {
-            for (int z = 0; z < diffToChunks[x].length; z++) {
-                if (x == 0 || z == 0 || x == diffToChunks.length - 1 || z == diffToChunks[x].length - 1) {
-                    if (x == 0 && z == 0) {
-                        diffToChunks[x][z] = maxHeights[x][z] - Math.min(outerLimits[0][1], outerLimits[1][0]);
-                    }
-                    else if (x == 0 && z == diffToChunks[x].length - 1) {
-                        diffToChunks[x][z] = maxHeights[x][z] - Math.min(outerLimits[0][outerLimits[x].length - 2], outerLimits[1][outerLimits[x].length - 1]);
-                    }
-                    else if (x == diffToChunks.length - 1 && z == 0) {
-                        diffToChunks[x][z] = maxHeights[x][z] - Math.min(outerLimits[outerLimits.length - 2][0], outerLimits[outerLimits.length - 1][1]);
-                    }
-                    else if (x == diffToChunks.length - 1 && z == diffToChunks[x].length - 1) {
-                        diffToChunks[x][z] = maxHeights[x][z] - Math.min(outerLimits[outerLimits.length - 2][outerLimits[x].length - 2], outerLimits[outerLimits.length - 1][outerLimits[x].length - 1]);
-                    }
-                    else if (x == 0) {
-                        diffToChunks[x][z] = maxHeights[x][z] - outerLimits[0][z];
-                    }
-                    else if (x == diffToChunks.length - 1) {
-                        diffToChunks[x][z] = maxHeights[x][z] - outerLimits[outerLimits.length - 1][z];
-                    }
-                    else if (z == 0) {
-                        diffToChunks[x][z] = maxHeights[x][z] - outerLimits[x][0];
-                    }
-                    else if (z == diffToChunks[x].length - 1) {
-                        diffToChunks[x][z] = maxHeights[x][z] - outerLimits[x][outerLimits[x].length - 1];
-                    }
-                }
-            }
-            System.out.println("This chunks lower left corner: " + maxHeights[0][15]);
-        }
-        //int count = 0;
-//        for (int x = 0; x < blocks.length; x++) {
-//            for (int z = 0; z < blocks[x][0].length; z++) {
-//                if (x == 0 || z == 0 || x == blocks.length - 1 || z == blocks[x].length - 1) {
-//                    if (diffToChunks[x][z] > 0) {
-//                        for (int y = maxHeights[x][z] - diffToChunks[x][z]; y < maxHeights[x][z]; y++) {
-//                            if (blocks[x][y][z].isActive() == false) {
-//                                blocks[x][y][z].activate();
-//                                count++;
-//                            }
-//                        }
-//                    }
-//                }
-//            }
-//        }
-        //System.out.println("Activated blocks in outer limits loop: " + count);
-        System.out.println("front block lower left corner should at height: " + outerLimits[0][1]);
-    }
 }
